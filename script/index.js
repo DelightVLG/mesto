783054ad0f4d3eb310b4920a7ae0d5d3bb874437
Sergei Kompanietc <delightvlg@gmail.com>
2020-07-06 20:58:12 +0300

    feat: eslint check

--- script/index.js
+++ script/index.js
@@ -1,35 +1,34 @@
-const openModalBtn = document.querySelector('.profile__edit-btn');
-const modal = document.querySelector('.modal');
+// eslint-disable-next-line no-undef
+const page = document.querySelector('.page');
+const openModalBtn = page.querySelector('.profile__edit-btn');
+const modal = page.querySelector('.modal');
 const closeModalBtn = modal.querySelector('.modal__close-btn');
 const modalForm = modal.querySelector('.modal__form');
-let profileName = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let modalFormName = modal.querySelector('.modal__input-txt_type_name')
-let modalFormJob = modal.querySelector('.modal__input-txt_type_job')
+const profileName = page.querySelector('.profile__title');
+const profileSubtitle = page.querySelector('.profile__subtitle');
+const modalFormName = modal.querySelector('.modal__input-txt_type_name');
+const modalFormJob = modal.querySelector('.modal__input-txt_type_job');
 
 function toggleModal() {
-    if (!modal.classList.contains('modal_is-open')) {
-        modalFormName.value = profileName.textContent;
-        modalFormJob.value = profileSubtitle.textContent;
-    }
-    modal.classList.toggle('modal_is-open');
+  if (!modal.classList.contains('modal_is-open')) {
+    modalFormName.value = profileName.textContent;
+    modalFormJob.value = profileSubtitle.textContent;
+  }
+  modal.classList.toggle('modal_is-open');
 }
 
 function formSubmitHandler(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    let editedName = modalFormName.value;
-    let editedJob = modalFormJob.value;
+  const editedName = modalFormName.value;
+  const editedJob = modalFormJob.value;
 
-    profileName.textContent = editedName;
-    profileSubtitle.textContent = editedJob;
+  profileName.textContent = editedName;
+  profileSubtitle.textContent = editedJob;
 
-    toggleModal();
+  toggleModal();
 }
 
-openModalBtn.addEventListener('click',toggleModal);
+openModalBtn.addEventListener('click', toggleModal);
 closeModalBtn.addEventListener('click', toggleModal);
 modalForm.addEventListener('submit', formSubmitHandler);
-
-
-
