99f99ca79071d9866f4f120c50e57cd71635d268
Sergei Kompanietc <delightvlg@gmail.com>
2020-08-05 19:34:45 +0300

    feat: add overlay and esc close

--- script/index.js
+++ script/index.js
@@ -1,40 +1,9 @@
 // eslint-disable-next-line no-undef
 const page = document.querySelector('.page');
-const initialCards = [
-  {
-    name: 'Бали',
-    link: 'https://images.unsplash.com/photo-1536152470836-b943b246224c?ixlib'
-      + '=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=978&q=80',
-  },
-  {
-    name: 'Рим',
-    link: 'https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib'
-      + '=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1586&q=80',
-  },
-  {
-    name: 'Швейцария',
-    link: 'https://images.unsplash.com/photo-1492119884860-63772f175a02?ixlib'
-      + '=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1981&q=80',
-  },
-  {
-    name: 'Кёльн',
-    link: 'https://images.unsplash.com/photo-1541791940715-e571aa1c3d32?ixlib'
-      + '=rb-1.2.1&auto=format&fit=crop&w=934&q=80',
-  },
-  {
-    name: 'Таллин',
-    link: 'https://images.unsplash.com/photo-1526154668476-dfe52ce0bfcc?ixlib'
-      + '=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-  },
-  {
-    name: 'Ватикан',
-    link: 'https://images.unsplash.com/photo-1573071510010-65996d895333?ixlib'
-      + '=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80',
-  },
-];
+
 const cardsTemplate = page.querySelector('.cards-template').content;
 const cardsContainer = page.querySelector('.elements'); // Контейнер всех карточек ul
-
+// TODO: Сделать рефакторинг форм. Тема: Доступ к значениям элементов форм.
 const editProfModal = page.querySelector('.modal_type_edit-profile');
 const editProfOpenBtn = page.querySelector('.profile__edit-btn');
 const editProfClsBtn = editProfModal.querySelector('.modal__close-btn');
@@ -55,10 +24,30 @@ const previewImage = previewModal.querySelector('.modal__preview-img');
 const previewSubtitle = previewModal.querySelector('.modal__preview-subtitle');
 const previewClsBtn = previewModal.querySelector('.modal__close-btn_type_preview');
 
+const modalList = Array.from(page.querySelectorAll('.modal'));
+
 // Функция открытия/закрытия модалок
 const toggleModal = (modalType) => {
   modalType.classList.toggle('modal_is-open');
+  page.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      const modalIsOpen = page.querySelector('.modal_is-open');
+      if (modalIsOpen) {
+        toggleModal(modalIsOpen);
+      }
+    }
+  });
 };
+
+// Закрытие модалок на оверлей
+modalList.forEach((modalType) => {
+  modalType.addEventListener('mousedown', (evt) => {
+    if (!evt.target.closest('.modal__container')) {
+      toggleModal(evt.currentTarget);
+    }
+  });
+});
+
 //  Функция открытия модалки ред.профиля
 const toggleEditProfModal = () => {
   if (!editProfModal.classList.contains('modal_is-open')) {
@@ -69,30 +58,30 @@ const toggleEditProfModal = () => {
 };
 
 // Обработчик кнопки лайка
-function likeListener(elem) {
+const likeListener = (elem) => {
   elem.querySelector('.elements__like-btn').addEventListener('click', (evt) => {
     evt.target.classList.toggle('elements__like-btn_is-active');
   });
-}
+};
 
 // Обработчик мусорки
-function cardDelete(elem) {
+const cardDelete = (elem) => {
   elem.querySelector('.elements__del-btn').addEventListener('click', (evt) => {
     evt.target.closest('li').remove();
   });
-}
+};
 
 // Обработчик превью картинки карточки
-function cardImgPreview(elem, src, subtitle = '') {
+const cardImgPreview = (elem, src, subtitle = '') => {
   elem.querySelector('.elements__img').addEventListener('click', () => {
     previewImage.src = src;
     previewSubtitle.textContent = subtitle;
     toggleModal(previewModal);
   });
-}
+};
 
 // Функция, описывающая логику создания внутренности карточки и навешивания лисенеров
-function createCard(link, name) {
+const createCard = (link, name) => {
   const cardElement = cardsTemplate.cloneNode(true);
   const elementImg = cardElement.querySelector('.elements__img');
 
@@ -106,14 +95,13 @@ function createCard(link, name) {
   cardImgPreview(cardElement, link, name);
 
   return cardElement;
-}
+};
 
 // Функция добавления карточки в контейнер
-function addElement(container, element) {
-  container.append(element);
-}
+const addElement = (container, element) => container.append(element);
 
 // Функция рендеринга начальных карточек при загрузке страницы
+// eslint-disable-next-line no-undef
 initialCards.forEach((elem) => {
   const cardElement = createCard(elem.link, elem.name);
   addElement(cardsContainer, cardElement);
@@ -147,7 +135,8 @@ const addPlaceSubmitHandler = (evt) => {
 };
 
 // Лисенеры на форму редактирования профиля
-editProfOpenBtn.addEventListener('click', () => toggleEditProfModal());
+// TODO: Сделать рефакторинг навешивания слушателей. Привести к едичному навешиванию
+editProfOpenBtn.addEventListener('click', toggleEditProfModal);
 editProfClsBtn.addEventListener('click', () => toggleModal(editProfModal));
 editProfModalForm.addEventListener('submit', editProfFormSubmitHandler);
 // Лисенеры на форму добавления карточки места
@@ -155,4 +144,4 @@ addPlaceOpenBtn.addEventListener('click', () => toggleModal(addPlaceModal));
 addPlaceClsBtn.addEventListener('click', () => toggleModal(addPlaceModal));
 addPlaceModal.addEventListener('submit', addPlaceSubmitHandler);
 // Лиснер на закрытие превью картинки карточки
-previewClsBtn.addEventListener('click', () => toggleModal(previewModal));
+previewClsBtn.addEventListener('click', () => toggleModal());
