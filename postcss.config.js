40d41143ab8af3dba3df217815518e06a00fe87a
Sergei Kompanietc <delightvlg@gmail.com>
2020-09-22 15:30:21 +0300

    feat: fix bug with autoprefixer, must be version 9.8.6, version 10.0.0 doesn't work!

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,9 @@
-// const autoprefixer = require('autoprefixer');
+const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
-    // autoprefixer,
+    autoprefixer,
     cssnano({ preset: 'default' }),
   ],
 };
-
-// Подключении плагина autoprefixer билд ломается с ошибкой true is not a PostCSS plugin
-// код скопирован с тренажера, все остальное собирается
